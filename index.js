305463eb2bcd768cea206cca4a5c410d9d56b4af
Petar Ozretic <petar.ozretic@locastic.com>
2024-03-12 11:42:17 +0100

    Connect to mongoDB (+env variables)

--- index.js
+++ index.js
@@ -1,8 +1,20 @@
 const express = require('express')
+const mongoose = require('mongoose');
+require('dotenv').config();
 const app = express()
 
 app.get('/', function (req, res) {
   res.send('Hello World')
 })
 
-app.listen(3000)
+
+
+mongoose.connect(`mongodb+srv://petar:${process.env.DB_PW}@nodedb.dzpay08.mongodb.net/Node-API?retryWrites=true&w=majority&appName=NodeDB`)
+.then(() => {
+  console.log('Connected to database!')
+  app.listen(3000, ()=> {
+    console.log('Server is running on port 3000')
+  })
+}).catch(() => {
+  console.log('Connection failed!')
+});
